675501470c2f50a5d6f063d011ddb73099bd077f
Lawlzer <kevindaspam@gmail.com>
2023-09-21 17:13:39 -0500

    Very quick prototype - Working

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import react, { useEffect } from "react";
+import Ascent from "./maps/ascent";
+
+// Agent
+// Util
+// Map
+// From
+// To
+
+// edit SVG, get click from SVG
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex items-center justify-center relative w-screen h-screen max-w-full">
+      <div className="absolute bg-gray-700 border-[15px] border-black p-20 rounded-full flex items-center justify-center">
+        <Ascent />
+      </div>
     </div>
   );
 }
