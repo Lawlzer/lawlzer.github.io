144b6bac4124d3e06497d272920e1a3b309666b1
Lawlzer <kevindaspam@gmail.com>
2023-10-08 08:20:11 -0500

    Prepare for implementing all maps and partial-support for all agents Implement SVGs for Ascent and Breeze Update all UI Buttons and make them prettier Replace useless assets with imgbb (i.ibb.co) links Fix areasFrom and areasTo (todo: make the functions global)

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,23 +1,36 @@
-export interface MapArea {
-  title: string;
+export interface MapArea<Titles extends String> {
+  title: Titles;
   x: number;
   y: number;
   width: number;
   height: number;
-
-  selected?: boolean;
 }
 
-type Agent = "Gekko";
-
-export interface Lineup {
+export interface Lineup<FromAreaTitles, ToAreaTitles> {
   map: string;
   agent: Agent;
   util: string;
 
-  fromTitle: string;
-  toTitle: string;
+  fromTitle: FromAreaTitles;
+  toTitle: ToAreaTitles;
 
   notes: string[];
-  imageLinks: string[];
+  imageSources: [] | [string, string] | [string];
 }
+
+export const imageMap = {
+  Gekko: "https://i.ibb.co/PjrxpnH/image.png",
+  Sova: "https://i.ibb.co/x33m7sW/Valorant-Sova-Guide-fotor-bg-remover-20230927223544.png",
+  "Mosh Pit": "https://i.ibb.co/LpJSDr1/moshPit.png",
+  "Shock Dart": "https://i.ibb.co/GcVMqj2/Screenshot-5-modified.png",
+  "Recon Dart": "https://i.ibb.co/GcVMqj2/Screenshot-5-modified.png", // https://i.ibb.co/5kpdtr1/mediamodifier-cropped-image.png
+} as const;
+
+export const agentUtilityMap = {
+  Gekko: ["Mosh Pit"],
+  Sova: ["Shock Dart", "Recon Dart"],
+} as const;
+export const agents = Object.keys(agentUtilityMap) as Agent[];
+
+export type Agent = keyof typeof agentUtilityMap;
+export type Utility = (typeof agentUtilityMap)[Agent][number];
