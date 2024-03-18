b93563087499a7fc1b6c0c70e7b22b4322c09981
Lawlzer <kevindaspam@gmail.com>
2024-03-18 17:23:32 -0500

    v1.0.12 release Implement more Breeze lineups

--- src/maps/breeze/lineups.ts
+++ src/maps/breeze/lineups.ts
@@ -2,10 +2,17 @@ import type { Lineup } from '../../types';
 
 import { Writeable, type MapArea } from '../../types';
 import image1 from './images/1.png';
+import image10 from './images/10.png';
+import image11 from './images/11.png';
+import image12 from './images/12.png';
 import image2 from './images/2.png';
 import image3 from './images/3.png';
 import image4 from './images/4.png';
 import image5 from './images/5.png';
+import image6 from './images/6.png';
+import image7 from './images/7.png';
+import image8 from './images/8.png';
+import image9 from './images/9.png';
 
 const tempAreasFrom = [
 	{
@@ -29,6 +36,20 @@ const tempAreasFrom = [
 		width: 12,
 		height: 16,
 	},
+	{
+		title: 'A CT Box',
+		x: 617,
+		y: 251,
+		width: 16,
+		height: 16,
+	},
+	{
+		title: 'A CT',
+		x: 557,
+		y: 129,
+		width: 16,
+		height: 16,
+	},
 ] as const; // We have to declare as const to type-check the titles.
 
 export const areasFrom: MapArea<string>[] = tempAreasFrom as Writeable<typeof tempAreasFrom>;
@@ -38,6 +59,13 @@ const tempAreasTo = [
 		title: 'B Default Plant',
 		x: 85,
 		y: 235,
+		width: 16,
+		height: 16,
+	},
+	{
+		title: 'A Safe Plant',
+		x: 744,
+		y: 372,
 		width: 16,
 		height: 16,
 	},
@@ -119,6 +147,91 @@ export const lineups: Lineup<FromAreaTitles, ToAreaTitles>[] = [
 				notes: ['2 Charges', '1 Bounce', 'Aim up-right of the first knot in the vine'],
 			},
 			{ image: image4, notes: ['Stand here'] },
+		],
+	},
+	{
+		agent: 'Gekko',
+		util: 'Mosh Pit',
+
+		fromTitle: 'A Shop',
+		toTitle: 'A Safe Plant',
+
+		imageStuff: [
+			{
+				image: image6,
+				notes: ['Stand against the box, then step back until you can see', 'Stand throw'],
+			},
+		],
+	},
+	{
+		agent: 'Gekko',
+		util: 'Mosh Pit',
+
+		fromTitle: 'A Mid Doors',
+		toTitle: 'A Safe Plant',
+
+		imageStuff: [
+			{
+				image: image7,
+				notes: ['Run throw, anywhere around here'],
+			},
+		],
+	},
+	{
+		agent: 'Gekko',
+		util: 'Mosh Pit',
+
+		fromTitle: 'A Mid Doors',
+		toTitle: 'A Safe Plant',
+
+		imageStuff: [
+			{
+				image: image8,
+				notes: ['Run 0.2s-delay throw'],
+			},
+			{ image: image9, notes: ['Aim here'] },
+		],
+	},
+	{
+		agent: 'Gekko',
+		util: 'Mosh Pit',
+
+		fromTitle: 'A CT Box',
+		toTitle: 'A Safe Plant',
+
+		imageStuff: [
+			{
+				image: image10,
+				notes: ['Full run-throw - Throw when spike is tapped. Aim somewhere here-ish'],
+			},
+		],
+	},
+	{
+		agent: 'Gekko',
+		util: 'Mosh Pit',
+
+		fromTitle: 'A CT',
+		toTitle: 'A Safe Plant',
+
+		imageStuff: [
+			{
+				image: image11,
+				notes: ['Jump insta-throw', 'Aim to the right of this blue spec'],
+			},
+		],
+	},
+	{
+		agent: 'Gekko',
+		util: 'Mosh Pit',
+
+		fromTitle: 'B Mid Boxes',
+		toTitle: 'B Safe Plant',
+
+		imageStuff: [
+			{
+				image: image12,
+				notes: ['Aim in the middle of this area'],
+			},
 		],
 	},
 ];
